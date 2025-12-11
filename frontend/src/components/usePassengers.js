6b1f8cc2033e5935b6e3ad2f72b56366c1a71f8b
Leo Alípio <leo.alipio@live.com>
2025-12-11 19:57:42 -0300

    add docker

--- frontend/src/components/usePassengers.js
+++ frontend/src/components/usePassengers.js
@@ -1,7 +1,7 @@
 import { use, useState } from "react";
 
 
-const fetchPassengers = fetch("http://127.0.0.1:5012/passengers")
+const fetchPassengers = fetch("http://localhost:8080/passengers")
 .then((response) => response.json());
 
 const usePassengers = () => {
